112350f9fea5f857a4492627d15767c8869486e3
santiago000777 <jjjjjxxxxx892@gmail.com>
2024-07-18 08:37:09 +0200

    zaloha trid

--- SDL2_PlatformerNew/MoveComponent.h
+++ SDL2_PlatformerNew/MoveComponent.h
@@ -1,13 +1,13 @@
 #pragma once
+#include "common.h"
+
 class MoveComponent {
 public:
 	TVec2 vector;
-	bool collision[4];
-	bool isDestroyble = false;
 
 	bool IsDestroyble();
-	virtual void HandleEvents();
-	virtual void Update(std::vector<Object*>* otherObjects, float delta);
+	virtual void HandleEvents() = 0;
+	virtual void Update(std::vector<MoveComponent*>* others, float delta);
 	enum eIndex : int {
 		LEFT = 0,
 		RIGHT,
@@ -15,7 +15,11 @@ public:
 		DOWN
 	};
 	SDL_Rect GetDstBox() const;
+
 private:
-	void CollisionPoint(std::vector<Object*>* otherObjects, float delta);
+	bool collision[4];
+	bool isDestroyble = false;
+	SDL_Rect dstBox;
+	void CollisionPoint(std::vector<MoveComponent*>* others, float delta);
 };
 
--- SDL2_PlatformerNew/RendererComponent.h
+++ SDL2_PlatformerNew/RendererComponent.h
@@ -1,13 +1,16 @@
 #pragma once
+#include "common.h"
+#include "Texture.h"
+
 class RendererComponent {
 public:
-	SDL_Rect dstBox;
+	
 	SDL_Rect srcBox;
 	SDL_Rect windowRect;
 
 	std::unique_ptr<SDL_Texture, void(*)(SDL_Texture*)> texture { nullptr, Texture::Delete };
 
-	virtual void Render();
+	virtual void Render() = 0;
 private:
 
 };
